6c41886cb6662f487049aa4d634b51287069d7d4
pavvelpotapov <pavelminsk1979@mail.ru>
2024-12-21 06:46:43 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import io, {Socket} from 'socket.io-client'
+//import axios, {options} from 'axios'
+
+const socketIoApi = {
+    socket: null as null | Socket,
+
+    createConnection() {
+        const options = {
+            extraHeaders: {
+                token: '123',
+                roomKey: 'test_room'
+            }
+        }
+        //тут подключение к бэку
+        this.socket = io('http://localhost:3010',options)
+
+        this.socket.on('connect',()=>{
+            console.log('connect')
+        })
+
+        this.socket.on('disconnect',(e)=>{
+            console.log(e)
+            console.log('disconnect')
+        })
+    }
+
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [text,setText]= useState('')
+    const [error,setError]= useState('')
+    const [eventPath,setEventPath]= useState('')
+
+    const connectSocket = () => {
+
+        socketIoApi.createConnection()
+
+        socketIoApi.socket?.on('andmin-path',(data:any)=>{
+            console.log('andmin-path',data)
+            setEventPath(JSON.stringify(data))
+        })
+
+        socketIoApi.socket?.on('unautorized',(data:any)=>{
+            console.log('error',data)
+            setEventPath(JSON.stringify(data))
+        })
+
+    }
+
+    const handleOnClick = () => {
+        socketIoApi.socket?.emit('admin-path',{value:text,id:'1'})
+    }
+
+    useEffect(()=>{
+        connectSocket()
+    },[])
+
+    return (
+        <div className="App">
+            <h1>ADMIN</h1>
+            <div>
+                <input
+                    value={text}
+                onChange={(e)=>{setText(e.currentTarget.value)}}/>
+                <button
+                onClick={handleOnClick}>ОТПРАВИТЬ</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
